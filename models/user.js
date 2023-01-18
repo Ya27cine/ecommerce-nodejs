268465cae1cbc0b4496d62866a52bbcc277858c7
KHELIFA Yassine <yacinemosta910@gmail.com>
2023-01-18 19:43:11 +0100

    Crypter le mot de Passe & User Schema

--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
+const { v4: uuidv4 } = require('uuid');
 const db = require("mongoose");
+const crypto = require("crypto");
+
 
 const userSchema = new db.Schema({
     name: {
@@ -14,7 +17,7 @@ const userSchema = new db.Schema({
         maxlength: 150,
         unique: true
     },
-    password: {
+    hashed_password: {
         type: String,
         required: true,
     },
@@ -34,3 +37,30 @@ const userSchema = new db.Schema({
         default: []
     }
 }, {timestamps: true})
+
+// Fields Virtual 
+userSchema.virtual('password')
+    .set( function(password){
+        this._password = password;
+        this.salt = uuidv4();
+        this.hashed_password = this.cryptPassword();
+    })
+    .get(function(){return this._password})
+
+// Methods Virtual 
+userSchema.methods = {
+    cryptPassword: function(password){
+        if( ! password ) return '';
+        try {
+            return crypto
+                    .createHmac('sha256', this.salt)
+                    .update(password)
+                    .digest('hex');
+        } catch (error) {
+            console.log( error )
+        }
+    },
+    authentication: function(plainText){
+        // return this.cryptPassword( plainText ) === this.hashed_password
+    }
+}
