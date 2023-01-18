3b7f41d2b0858fa3a073b174843475b984da2fcc
KHELIFA Yassine <yacinemosta910@gmail.com>
2023-01-18 18:20:43 +0100

    Système de Routage

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const userRoutes = require("./routes/users");
 const db = require("mongoose");
 const server = express();
 require("dotenv").config();
 
+// Routes Middleware :
+server.use("/api/users", userRoutes);
+
+// Server
 const port = process.env.PORT || 3000;
 server.listen( port, () => console.log("App is running on port ",port))
 
