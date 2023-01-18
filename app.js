091a328be0b7eef36294f040e965de8bec3f2c6d
KHELIFA Yassine <yacinemosta910@gmail.com>
2023-01-18 18:01:18 +0100

    Connection avec MongoDB

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const db = require("mongoose");
 const server = express();
 require("dotenv").config();
 
 const port = process.env.PORT || 3000;
 server.listen( port, () => console.log("App is running on port ",port))
+
+// Connect DB
+db.set("strictQuery", false);
+db.connect(process.env.dataBaseUrl,{
+    useNewUrlParser: true,  // no longer necessary
+    })
+    .then(() => console.log("DB connected !"))
+    .catch((e) => console.log("not connect to database"))
